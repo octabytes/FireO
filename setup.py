c0529aa3a4960ea957be7291012c65439a1aec5c
AxeemHaider <axeemhaider@gmail.com>
2019-09-29 19:06:30 +0500

    Very first thing

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='fireo',
+    version='0.0.1',
+    description='FireStore ORM',
+    py_modules=["helloworld"],
+    package_dir={'': 'src'}
+)
---
+++ src/helloworld.py
@@ -0,0 +1,3 @@
+
+def say_hello():
+    print("This lib is under development")
