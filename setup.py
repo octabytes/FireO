ce836ba28b7948bed744937b74d6a8cdc47e94a3
AxeemHaider <axeemhaider@gmail.com>
2019-10-01 12:54:04 +0500

    Adding license

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
         "Operating System :: OS Independent",
         "Intended Audience :: Developers"
     ],
