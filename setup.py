bb56a5ad0afaa2b60b73dc6c0f424f811f9b499d
AxeemHaider <axeemhaider@gmail.com>
2019-10-02 15:05:07 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,70 @@
-from setuptools import setup
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
     name='fireo',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
     version='0.0.3',
+
     description="FireO ORM is specifically designed for the Google's Firestore",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/octabytes/FireO.git",
+
+    # The project's main homepage.
+    url="https://github.com/octabytes/FireO",
+
+    # Author details
     author="OctaByte",
     author_email="Dev@octabyte.io",
+
+    # Choose your license
+    license='GPLv3',
+
+    # See https://pypi.org/classifiers/
     classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+
+        # Pick your license as you wish (should match "license" above)
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: OS Independent",
-        "Intended Audience :: Developers"
+        "Programming Language :: Python :: 3.7"
     ],
-    py_modules=["helloworld"],
-    package_dir={'': 'src'}
+
+    # What does your project relate to?
+    keywords='Python Firestore Models ORM Google Cloud Firebase',
+
+    # Add all packages except tests
+    packages=find_packages(exclude=['tests']),
+
+    package_dir={'': 'src'},
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=['google-cloud-firestore'],
 )
