42755c9a5dc037dd4b2faf8305a5d2ed11354348
AxeemHaider <axeemhaider@gmail.com>
2019-10-15 15:43:47 +0500

    Nested model field created

---
+++ src/fireo/fields/nested_model.py
@@ -0,0 +1,21 @@
+from fireo.fields import errors
+from fireo.fields.base_field import Field
+
+
+class NestedModel(Field):
+    """Model inside another model"""
+
+    def __init__(self, model, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Check nested model class is subclass for Model
+        from fireo.models import Model
+        if not issubclass(model, Model):
+            raise errors.NestedModelTypeError(f'Nested model {model.__name__} must be inherit from Model class')
+        self.nested_model = model
+
+    def valid_model(self, model_instance):
+        """Check nested model and passing model is same"""
+        if self.nested_model == model_instance.__class__:
+            return True
+        raise errors.NestedModelTypeError(f'Invalid nested model type. Field "{self.name}" required value type '
+                                          f'{self.nested_model.__name__}, but got {model_instance.__class__.__name__}')
