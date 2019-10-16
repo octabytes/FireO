c70733e06df8cce12f2aee5f477021e7e66a18fe
AxeemHaider <axeemhaider@gmail.com>
2019-10-16 12:28:30 +0500

    writting test for parent key

---
+++ src/tests/parent_key_test.py
@@ -0,0 +1,146 @@
+from fireo import models as mdl
+
+
+class User(mdl.Model):
+    id = mdl.IDField()
+    name = mdl.TextField()
+
+
+class Student(mdl.Model):
+    id = mdl.IDField()
+    address = mdl.TextField()
+
+
+def test_parent_key_with_id_field():
+    u = User()
+    u.id = 'test_parent_key_with_id_field'
+    u.name = 'testing parent key with id field'
+    u.save()
+
+    s1 = Student(parent=u.key)
+    s1.id = 'student_id_in_test_parent_key'
+    s1.address = 'testing parent student key'
+    s1.save()
+
+    s2 = Student.collection.get(s1.key)
+    assert s1.id == s2.id
+    assert s1.key == s2.key
+
+
+class User1(mdl.Model):
+    user_id = mdl.IDField()
+    name = mdl.TextField()
+
+
+class Student1(mdl.Model):
+    student_id = mdl.IDField()
+    address = mdl.TextField()
+
+
+def test_parent_key_with_custom_field():
+    u = User()
+    u.user_id = 'test_parent_key_with_custom_field'
+    u.name = 'testing parent key with_custom_field'
+    u.save()
+
+    s1 = Student(parent=u.key)
+    s1.student_id = 'student_id_test_parent_key_with_custom_field'
+    s1.address = 'testing parent student keywith_custom_field'
+    s1.save()
+
+    s2 = Student.collection.get(s1.key)
+    assert s1.student_id == s2.student_id
+    assert s1.key == s2.key
+
+    assert s1.id is None
+    assert s2.id is None
+
+
+def test_parent_key_custom_field_without_value():
+    u = User()
+    u.name = 'testing parent key with_custom_field'
+    u.save()
+
+    s1 = Student(parent=u.key)
+    s1.address = 'testing parent student keywith_custom_field'
+    s1.save()
+
+    s2 = Student.collection.get(s1.key)
+    assert s1.student_id == s2.student_id
+    assert s1.key == s2.key
+
+    assert s1.id is None
+    assert s2.id is None
+
+
+def test_parent_key_without_value():
+    u = User()
+    u.id = 'test_parent_key_without_value'
+    u.name = 'testing parent key without value'
+    u.save()
+
+    s1 = Student(parent=u.key)
+    s1.id = 'student_id_in_test_parent_key_without_value'
+    s1.address = 'testing parent student key without value'
+    s1.save()
+
+    s2 = Student.collection.get(s1.key)
+    assert s1.id == s2.id
+    assert s1.key == s2.key
+
+
+class Company(mdl.Model):
+    name = mdl.TextField()
+
+
+class Employee(mdl.Model):
+    address = mdl.TextField()
+
+
+def test_parent_key_without_id_field():
+    c = Company()
+    c.name = 'testing parent key without id field'
+    c.save()
+
+    e1 = Employee(parent=c.key)
+    e1.address = 'testing parent student key without field'
+    e1.save()
+
+    e2 = Student.collection.get(e1.key)
+    assert e1.id == e2.id
+    assert e1.key == e2.key
+
+
+def test_parent_key_with_value():
+    c = Company()
+    c.id = 'test_parent_key_with_value'
+    c.name = 'testing parent key with value'
+    c.save()
+
+    e1 = Employee(parent=c.key)
+    e1.id = 'student_test_parent_key_with_values'
+    e1.address = 'testing parent student key with value'
+    e1.save()
+
+    e2 = Student.collection.get(e1.key)
+    assert e1.id == e2.id
+    assert e1.key == e2.key
+
+
+def test_parent_key_with_id_name():
+    c = Company()
+    c.id = 'test_parent_key_with_id_name'
+    c.name = 'testing parent key with value'
+    c.save()
+
+    e1 = Employee(parent=c.key)
+    e1.id = 'student_test_parent_key_with_id_name'
+    e1.address = 'testing parent student key with value'
+    e1.save()
+
+    e2 = Student.collection.get(e1.key)
+    assert e1.id == e2.id
+    assert e1.key == e2.key
+
+    assert e1.id != 'test_parent_key_with_id_name'
+    assert e2.id != 'test_parent_key_with_id_name'
--- src/tests/simple_id_key_test.py
+++ src/tests/simple_id_key_test.py
@@ -1,7 +1,92 @@
 from fireo import models as mdl
 
 
-def test_id():
-    a = 20
+class User(mdl.Model):
+    id = mdl.IDField()
+    name = mdl.TextField()
 
-    assert a == 20
+
+def test_id():
+    u = User()
+    u.id = 'test_id_field'
+    u.name = 'testing_id_field'
+    u.save()
+
+    u2 = User.collection.get(u.key)
+    assert u2.id == u.id
+    assert u2.name == u.name
+    assert u2.key == u.key
+
+
+def test_without_id():
+    u1 = User()
+    u1.name = 'test_without_id'
+    u1.save()
+
+    u2 = User.collection.get(u1.key)
+    assert u1.id == u2.id
+    assert u1.name == u2.name
+    assert u1.key == u2.key
+
+
+class Student(mdl.Model):
+    user_id = mdl.IDField()
+    name = mdl.TextField()
+
+
+def test_id_with_different_name():
+    s1 = Student()
+    s1.user_id = 'test_id_with_different_name'
+    s1.name = 'testing_id_with_different_name'
+    s1.save()
+
+    s2 = Student.collection.get(s1.key)
+    assert s1.user_id == s2.user_id
+    assert s1.name == s2.name
+    assert s1.key == s2.key
+
+    assert s1.id is None
+    assert s2.id is None
+
+
+def test_id_without_value():
+    s1 = Student()
+    s1.name = 'testing_id_with_different_name'
+    s1.save()
+
+    s2 = Student.collection.get(s1.key)
+    assert s1.user_id == s2.user_id
+    assert s1.name == s2.name
+    assert s1.key == s2.key
+
+    assert s1.id is None
+    assert s2.id is None
+
+
+class Employee(mdl.Model):
+    name = mdl.TextField()
+
+
+def test_without_id_field():
+    e1 = Employee()
+    e1.name = 'test_without_id_field'
+    e1.save()
+
+    e2 = Employee.collection.get(e1.key)
+    assert e1.id == e2.id
+    assert e1.name == e2.name
+    assert e1.key == e2.key
+
+
+def test_without_id_field_but_giving_id():
+    e1 = Employee()
+    e1.id = 'test_without_id_but_giving_id'
+    e1.name = 'test_without_id_but_give_some_id'
+    e1.save()
+
+    assert e1.id != 'test_without_id_but_giving_id'
+
+    e2 = Employee.collection.get(e1.key)
+    assert e2.id != 'test_without_id_but_giving_id'
+    assert e1.name == e2.name
+    assert e1.key == e2.key
