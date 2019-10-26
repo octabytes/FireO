d48f008d32ad483d5410960c4b4b3976a495a336
AxeemHaider <axeemhaider@gmail.com>
2019-10-26 14:15:12 +0500

    Pre release 0.0.5

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.4',
+    version='0.0.5',
 
     description="FireO ORM is specifically designed for the Google's Firestore",
     long_description=long_description,
@@ -38,7 +38,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
 
         # Indicate who your project is intended for
         "Intended Audience :: Developers",
