f1ed4b9dec4cab19c3ada1c766fc8cd1137294f8
AxeemHaider <axeemhaider@gmail.com>
2019-10-26 12:57:58 +0500

    Add delete test

---
+++ src/tests/delete_model_test.py
@@ -0,0 +1,65 @@
+from fireo.fields import TextField, NumberField
+from fireo.models import Model
+
+
+class DeleteModelUser(Model):
+    name = TextField()
+
+
+class DeleteModelChild(Model):
+    age = NumberField()
+
+
+def test_simple_delete():
+    d = DeleteModelUser(name="Name")
+    d.save()
+
+    DeleteModelUser.collection.delete(d.key)
+
+    d2 = DeleteModelUser.collection.get(d.key)
+
+    assert d2 is None
+
+
+def test_multi_delete():
+    d = DeleteModelUser(name="Name1")
+    d.save()
+    d = DeleteModelUser(name="Name2")
+    d.save()
+
+    DeleteModelUser.collection.delete()
+
+    d2 = DeleteModelUser.collection.fetch()
+
+    assert next(d2, None) is None
+
+
+def test_parent_delete():
+    d = DeleteModelUser(name="Name")
+    d.save()
+    c = DeleteModelChild(parent=d.key)
+    c.age = 26
+    c.save()
+
+    DeleteModelChild.collection.delete(c.key)
+
+    c2 = DeleteModelChild.collection.get(c.key)
+
+    assert c2 is None
+
+
+def test_multi_parent_delete():
+    d = DeleteModelUser(name="Name")
+    d.save()
+    c = DeleteModelChild(parent=d.key)
+    c.age = 26
+    c.save()
+    c = DeleteModelChild(parent=d.key)
+    c.age = 27
+    c.save()
+
+    DeleteModelChild.collection.parent(d.key).delete()
+
+    c2 = DeleteModelChild.collection.parent(d.key).fetch()
+
+    assert next(c2, None) is None
