f57e85b01e9b6ee0d136f578ffec873678886a99
AxeemHaider <axeemhaider@gmail.com>
2019-10-26 21:54:33 +0500

    First stable release v0.1.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.7',
+    version='0.1.0',
 
     description="FireO ORM is specifically designed for the Google's Firestore",
     long_description=long_description,
@@ -38,7 +38,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
 
         # Indicate who your project is intended for
         "Intended Audience :: Developers",
