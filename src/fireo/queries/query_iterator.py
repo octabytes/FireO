663b0da4813eae868fbdac57df32e5f946fc8420
AxeemHaider <axeemhaider@gmail.com>
2019-11-09 15:29:34 +0500

    Next fetch complete

--- src/fireo/queries/query_iterator.py
+++ src/fireo/queries/query_iterator.py
@@ -1,5 +1,5 @@
+import itertools
 from fireo.queries import query_wrapper
-from fireo.queries.filter_query import FilterQuery
 
 
 class QueryIterator:
@@ -7,16 +7,49 @@ class QueryIterator:
 
     Iterate each document that is coming from Firestore and wrap it to the model
     QueryIterator also contain the other information for example Firestore query, cursor etc
+
+    Methods
+    -------
+    fetch_next():
+        Fetch next results
     """
 
-    def __init__(self, query: FilterQuery):
+    def __init__(self, query):
         self.query = query
         self.docs = query.query().stream()
+        # Get offset for next fetch
+        self.offset = query.n_limit
+        # Hold the last doc for next fetch
+        self.last_doc = None
+        self.fetch_end = False
+
+    def __iter__(self):
+        return self
 
     def __next__(self):
         doc = next(self.docs, None)
         if doc:
+            # Suppose this is the last doc
+            self.last_doc = doc
             m = query_wrapper.ModelWrapper.from_query_result(self.query.model, doc)
             m.update_doc = self.query._update_doc_key(m)
             return m
+        self.fetch_end = True
         raise StopIteration
+
+    def next_fetch(self, limit=None):
+        """Fetch next results"""
+
+        # Make sure limit is apply in previous query otherwise there is no need to fetch next
+        if self.offset:
+            # check if fetch end then use last doc otherwise use the offset
+            if self.fetch_end:
+                q = self.query.query().start_after(self.last_doc)
+            else:
+                q = self.query.query().offset(self.offset)
+
+            # Apply new Limit if there is any
+            if limit:
+                q = q.limit(limit)
+
+            self.docs = itertools.chain(self.docs, q.stream())
