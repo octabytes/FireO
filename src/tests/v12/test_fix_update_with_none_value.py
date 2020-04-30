3bf33f5ea4e722c09958ecd31d059ae7f4c3568c
Andrii Kuzmin <jack.cvr@gmail.com>
2020-04-30 16:39:14 +0300

    refix issue #48: field_attribute validation

--- src/tests/v12/test_fix_update_with_none_value.py
+++ src/tests/v12/test_fix_update_with_none_value.py
@@ -1,21 +1,40 @@
+import pytest
+
 from fireo.models import Model
 from fireo.fields.text_field import TextField
 from fireo.fields.number_field import NumberField
+from fireo.fields.errors import NumberRangeError
 
 
 def test_fix_issue_48():
     class Person(Model):
         name = TextField(required=True, max_length=10)
-        type = NumberField(required=True, int_only=True)
+        type = NumberField(required=True, int_only=True, range=(1, 2))
 
-
-    person = Person.collection.create(name="test", type=1)
+    # test TextField
+    person = Person.collection.create(name='test', type=1)
 
     assert person.name == 'test'
     assert person.type == 1
 
-    person.type = 123
+    person.type = 2
     person.update()
 
     assert person.name == 'test'
-    assert person.type == 123
+    assert person.type == 2
+
+    # test NumberField
+    person = Person.collection.create(name='test', type=1)
+
+    assert person.name == 'test'
+    assert person.type == 1
+
+    person.name = 'test2'
+    person.update()
+
+    assert person.name == 'test2'
+    assert person.type == 1
+
+    person.type = 3
+    with pytest.raises(NumberRangeError):
+        person.update()
