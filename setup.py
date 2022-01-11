e06737fa86265fb324804f04f8af17fd7b942e25
AxeemHaider <axeemhaider@gmail.com>
2022-01-11 10:58:20 +0500

    update package version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.4.7',
+    version='1.4.8',
 
     description="FireO ORM is specifically designed for the Google's Firestore.",
     long_description=long_description,
