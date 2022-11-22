e59a1a92b85df026063fcd73a62723eaeb055beb
AxeemHaider <axeemhaider@gmail.com>
2022-11-22 10:47:28 +0500

    test: update test adding compound queries with id filter issue #160

--- src/tests/v1.6/test_issue_160.py
+++ src/tests/v1.6/test_issue_160.py
@@ -1,6 +1,6 @@
 import pytest
 from fireo.fields.errors import AttributeTypeError
-from fireo.fields import TextField, IDField
+from fireo.fields import TextField, IDField, BooleanField
 from fireo.models import Model
 
 # first try with default field name
@@ -92,3 +92,26 @@ def test_issue_160_should_able_to_get_with_key():
         assert num_results == 1
     except Exception:
         assert True == False
+
+def test_issue_160_with_compound_queries():
+    class TestIssue160Model(Model):
+        name = TextField()
+        is_deleted = BooleanField()
+
+    test1 = TestIssue160Model(name='test1', is_deleted=False)
+    test1.save()
+
+    test2 = TestIssue160Model(name='test2', is_deleted=True)
+    test2.save()
+
+    test3 = TestIssue160Model(name='test3', is_deleted=True)
+    test3.save()
+
+    try:
+        docs = TestIssue160Model.collection.filter("id", "in", [test1.id, test2.id]).filter("is_deleted", "==", False).fetch()
+        doc = next(docs)
+
+        assert doc.id == test1.id
+        assert doc.name == test1.name
+    except Exception:
+        assert False == True
