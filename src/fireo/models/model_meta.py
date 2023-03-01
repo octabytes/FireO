60fe3d616d2d98e88efb1d368aefd355fe69b717
Adrian Dankiv <adr-007@ukr.net>
2023-03-01 21:59:05 +0000

    Allow change base class for Meta

--- src/fireo/models/model_meta.py
+++ src/fireo/models/model_meta.py
@@ -1,141 +1,11 @@
+from typing import Type
+
 from fireo import fields
 from fireo.fields import Field
 from fireo.fields.errors import FieldNotFound, MissingFieldOptionError
 from fireo.managers import managers
 from fireo.models.errors import DuplicateIDField, NonAbstractModel, UnSupportedMeta
 from fireo.utils import utils
-
-
-class ModelMeta(type):
-    """Create fields, manager and other stuff for Models
-
-    Add `_meta` attribute to each Model which hold all the information about fields, managers and any
-    other stuff related to model. Also responsible to generate collection name from model.
-
-    Methods
-    -------
-    __new__(mcs, name, base, attrs):
-        Create modified type for model class
-    """
-
-    _meta: 'Meta'
-
-    def __new__(mcs, name, base, attrs):
-        """Create modified type for model class
-
-        Convert fields, managers and other stuff into `Meta` class and attached it with model class
-
-        Attributes
-        ----------
-        _meta : Meta()
-            Hold all information about you model, can be accessible via class **cls._meta**
-
-        collection_name : str
-            Name of collection which is saved in database if user not provided any collection name
-            then `Meta` class generate it from Model name and attach it with class can be accessible
-            via class **cls.collection_name**
-
-        Returns
-        -------
-            Modified type for model class
-        """
-        cls = super().__new__(mcs, name, base, attrs)
-
-        # Create instance of Meta class and set it to
-        # Model class as _meta attribute
-        parent_meta = getattr(cls, '_meta', None)
-        if parent_meta is not None:
-            _meta = Meta.from_parent_meta(cls, parent_meta)
-        else:
-            _meta = Meta(cls)
-        setattr(cls, '_meta', _meta)
-
-        # check user specify any additional meta data for this model or not
-        # For example collection name etc if there is not then assign None to it
-        if 'Meta' not in attrs:
-            cls.Meta = None
-
-        # Get all fields from model and save them into `_meta.field_list()`
-        # and then attach this `_meta` to model class
-        for name, field in cls.__dict__.items():
-            if isinstance(field, type) and name == 'Meta':
-                _meta.set_user_defined_meta(field)
-            if isinstance(field, (managers.Manager, fields.Field)):
-                field.contribute_to_model(cls, name)
-
-        # Get base Model if they are abstract then add these models field
-        # in child classes
-        #
-        # For example:
-        #   class AbstractModel(Model):
-        #       name = TextField()
-        #
-        #       class Meta:
-        #           abstract = True
-        #
-        #   class User(AbstractModel):
-        #       age = NumberFiled()
-        #
-        #   class Student(AbstractModel):
-        #       age = NumberFiled()
-        #
-        # Both Model user and student now contains the name field
-        #
-        for b in base:
-            if not hasattr(b, '_meta'):
-                continue
-            if not b._meta.abstract:
-                raise NonAbstractModel(f'Model "{cls.__name__}" can not inherit from non abstract '
-                                       f'model "{b.__name__}"')
-            for name, field in b._meta.field_list.items():
-                field.contribute_to_model(cls, name)
-
-        if not cls._meta.abstract and cls._meta.id is None:
-            if 'id' in cls._meta.field_list:
-                raise DuplicateIDField(
-                    f'Model "{cls.__name__}" can not have field named "id" '
-                    f'because it is reserved for the document ID. '
-                    f'Please use a different field name or define custom IDField'
-                )
-            cls.id = fields.IDField()
-            cls.id.name = 'id'
-            cls.id.contribute_to_model(cls, 'id')
-
-        # Set collection name to model class that is generated from
-        # Model class this name can be user defined or auto generated
-        # from model class
-        #
-        # For example:
-        #   class UserCollection:
-        #       name = TextField()
-        #
-        # if user not defined collection name then it will auto generate
-        # for this class
-        #
-        # UserCollection will become user_collection
-        setattr(cls, "collection_name", cls._meta.collection_name)
-
-        mcs._generate_column_names(_meta.column_name_generator, attrs)
-
-        return cls
-
-    @staticmethod
-    def _generate_column_names(column_name_generator, attrs):
-        """Generate column names for fields.
-
-        Can be used to generate camelCase column names for fields.
-        """
-        if column_name_generator is None:
-            return
-
-        for key, value in attrs.items():
-            if not isinstance(value, Field):
-                continue
-
-            if 'column_name' in value.raw_attributes:
-                continue
-
-            value.raw_attributes['column_name'] = column_name_generator(value.name)
 
 
 class Meta:
@@ -228,16 +98,26 @@ class Meta:
         If any model is inherit from non abstract model
     """
 
-    def __init__(
-        self,
-        model_cls,
-        default_manager_cls=managers.Manager,
-        missing_field='merge',
-        ignore_none_field=True,
-        to_lowercase=False,
-        column_name_generator=None,
-        collection_name_generator=utils.collection_name,
-    ):
+    supported_fields = [
+        'abstract',
+        'collection_name',
+        'collection_name_generator',
+        'column_name_generator',
+        'default_manager_cls',
+        'ignore_none_field',
+        'missing_field',
+        'to_lowercase',
+    ]
+    inherited_fields = [
+        'collection_name_generator',
+        'column_name_generator',
+        'default_manager_cls',
+        'ignore_none_field',
+        'missing_field',
+        'to_lowercase',
+    ]
+
+    def __init__(self, model_cls):
         self.id = None  # Model id if user specify otherwise just None will generate late by firestore automatically
         self.field_list = {}  # Hold all the model fields
 
@@ -247,19 +127,12 @@ class Meta:
         self._collection_name = None
         self.abstract = False
         self.model_cls = model_cls
-        self.default_manager_cls = default_manager_cls
-        self.missing_field = missing_field
-        self.ignore_none_field = ignore_none_field
-        self.to_lowercase = to_lowercase
-        self.column_name_generator = column_name_generator
-        self.collection_name_generator = collection_name_generator
-
-        # Attached manager to model class
-        # later on manager can be accessible via class `collection` attribute
-        # like this **cls.collection**
-        if 'collection' not in model_cls.__dict__:
-            manager = self.default_manager_cls()
-            manager.contribute_to_model(model_cls)
+        self.default_manager_cls = managers.Manager
+        self.missing_field = 'merge'
+        self.ignore_none_field = True
+        self.to_lowercase = False
+        self.column_name_generator = None
+        self.collection_name_generator = utils.collection_name
 
     @property
     def collection_name(self):
@@ -271,18 +144,6 @@ class Meta:
     @collection_name.setter
     def collection_name(self, value):
         self._collection_name = value
-
-    @classmethod
-    def from_parent_meta(cls, model_cls, parent_meta: 'Meta') -> 'Meta':
-        return cls(
-            model_cls=model_cls,
-            default_manager_cls=parent_meta.default_manager_cls,
-            missing_field=parent_meta.missing_field,
-            ignore_none_field=parent_meta.ignore_none_field,
-            to_lowercase=parent_meta.to_lowercase,
-            column_name_generator=parent_meta.column_name_generator,
-            collection_name_generator=parent_meta.collection_name_generator,
-        )
 
     def add_model_id(self, field):
         """ Attach user define id to model
@@ -382,6 +243,10 @@ class Meta:
         if self.missing_field == 'raise_error':
             raise FieldNotFound(f'Field "{name}" not found in model "{self.model_cls.__name__}"')
 
+    def set_field_from_parent(self, parent_meta: 'Meta') -> 'Meta':
+        for field in self.inherited_fields:
+            setattr(self, field, getattr(parent_meta, field))
+
     def set_user_defined_meta(self, user_meta):
         """Set user defined meta attributes for model
 
@@ -438,3 +303,142 @@ class Meta:
                         f'Possible values are ignore, merge, raise_error')
             else:
                 setattr(self, name, val)
+
+
+class ModelMeta(type):
+    """Create fields, manager and other stuff for Models
+
+    Add `_meta` attribute to each Model which hold all the information about fields, managers and any
+    other stuff related to model. Also responsible to generate collection name from model.
+
+    Methods
+    -------
+    __new__(mcs, name, base, attrs):
+        Create modified type for model class
+    """
+
+    _meta: Meta
+    _meta_cls: Type[Meta] = Meta
+
+    def __new__(mcs, name, base, attrs):
+        """Create modified type for model class
+
+        Convert fields, managers and other stuff into `Meta` class and attached it with model class
+
+        Attributes
+        ----------
+        _meta : Meta()
+            Hold all information about you model, can be accessible via class **cls._meta**
+
+        collection_name : str
+            Name of collection which is saved in database if user not provided any collection name
+            then `Meta` class generate it from Model name and attach it with class can be accessible
+            via class **cls.collection_name**
+
+        Returns
+        -------
+            Modified type for model class
+        """
+        cls = super().__new__(mcs, name, base, attrs)
+
+        # Create instance of Meta class and set it to
+        # Model class as _meta attribute
+        parent_meta = getattr(cls, '_meta', None)
+        _meta = cls._meta_cls(cls)
+        if parent_meta is not None:
+            _meta.set_field_from_parent(parent_meta)
+        setattr(cls, '_meta', _meta)
+
+        # check user specify any additional meta data for this model or not
+        # For example collection name etc if there is not then assign None to it
+        if 'Meta' not in attrs:
+            cls.Meta = None
+
+        # Get all fields from model and save them into `_meta.field_list()`
+        # and then attach this `_meta` to model class
+        for name, field in cls.__dict__.items():
+            if isinstance(field, type) and name == 'Meta':
+                _meta.set_user_defined_meta(field)
+            if isinstance(field, (managers.Manager, fields.Field)):
+                field.contribute_to_model(cls, name)
+
+        # Get base Model if they are abstract then add these models field
+        # in child classes
+        #
+        # For example:
+        #   class AbstractModel(Model):
+        #       name = TextField()
+        #
+        #       class Meta:
+        #           abstract = True
+        #
+        #   class User(AbstractModel):
+        #       age = NumberFiled()
+        #
+        #   class Student(AbstractModel):
+        #       age = NumberFiled()
+        #
+        # Both Model user and student now contains the name field
+        #
+        for b in base:
+            if not hasattr(b, '_meta'):
+                continue
+            if not b._meta.abstract:
+                raise NonAbstractModel(f'Model "{cls.__name__}" can not inherit from non abstract '
+                                       f'model "{b.__name__}"')
+            for name, field in b._meta.field_list.items():
+                field.contribute_to_model(cls, name)
+
+        if not cls._meta.abstract and cls._meta.id is None:
+            if 'id' in cls._meta.field_list:
+                raise DuplicateIDField(
+                    f'Model "{cls.__name__}" can not have field named "id" '
+                    f'because it is reserved for the document ID. '
+                    f'Please use a different field name or define custom IDField'
+                )
+            cls.id = fields.IDField()
+            cls.id.name = 'id'
+            cls.id.contribute_to_model(cls, 'id')
+
+        # Set collection name to model class that is generated from
+        # Model class this name can be user defined or auto generated
+        # from model class
+        #
+        # For example:
+        #   class UserCollection:
+        #       name = TextField()
+        #
+        # if user not defined collection name then it will auto generate
+        # for this class
+        #
+        # UserCollection will become user_collection
+        setattr(cls, "collection_name", cls._meta.collection_name)
+
+        # Attached manager to model class
+        # later on manager can be accessible via class `collection` attribute
+        # like this **cls.collection**
+        if 'collection' not in cls.__dict__:
+            manager = _meta.default_manager_cls()
+            manager.contribute_to_model(cls)
+
+        mcs._generate_column_names(_meta.column_name_generator, attrs)
+
+        return cls
+
+    @staticmethod
+    def _generate_column_names(column_name_generator, attrs):
+        """Generate column names for fields.
+
+        Can be used to generate camelCase column names for fields.
+        """
+        if column_name_generator is None:
+            return
+
+        for key, value in attrs.items():
+            if not isinstance(value, Field):
+                continue
+
+            if 'column_name' in value.raw_attributes:
+                continue
+
+            value.raw_attributes['column_name'] = column_name_generator(value.name)
