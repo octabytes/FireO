8be5aaad85ba50947e4ddaa648ca24779e279eae
AxeemHaider <axeemhaider@gmail.com>
2023-03-02 10:24:12 +0500

    version: update google-cloud-firestore version to 2.10.0

--- setup.py
+++ setup.py
@@ -69,5 +69,5 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['google-cloud-firestore>=2.2.0,<=2.3.4'],
+    install_requires=['google-cloud-firestore==2.10.0'],
 )
